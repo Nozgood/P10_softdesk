71efcd485afce7811cd17094f07c845e36854c31
nowfeel <nowfeel@bliink.io>
2023-12-15 12:49:23 +0100

    refactor: improve create method by using a validator

--- softdesk_management/serializers.py
+++ softdesk_management/serializers.py
@@ -62,7 +62,7 @@ class ContributorSerializer(ModelSerializer):
 
 class IssueSerializer(ModelSerializer):
     project_id = IntegerField(min_value=1)
-    attribution_id = IntegerField(min_value=1)
+    attribution_id = IntegerField(min_value=1, allow_null=True, required=False)
 
     class Meta:
         model = models.Issue
@@ -76,45 +76,65 @@ class IssueSerializer(ModelSerializer):
             "attribution_id",
         ]
 
-    def create(self, validated_data):
-        reporter_user = self.context["request"].user
-        project_id = validated_data["project_id"]
+    def validate(self, attrs):
+        reporter = self.context["request"].user
+        project_id = attrs.get("project_id")
+        attribution_id = attrs.get("attribution_id")
 
-        try:
-            project = models.Project.objects.get(
-                pk=project_id
-            )
-        except models.Project.DoesNotExist:
-            raise ValidationError(
-                "the project you give does not exist"
-            )
+        if not models.Project.objects.filter(
+            pk=project_id
+        ).exists():
+            raise ValidationError("this project does not exist")
 
         if not models.Contributor.objects.filter(
             project_id=project_id,
-            user=reporter_user
+            user=reporter
         ).exists():
             raise ValidationError(
                 "you must be a contributor of this project to create an issue"
             )
 
-        try:
-            attribution_user = users_models.User.objects.get(
-                pk=validated_data["attribution_id"]
+        if attribution_id is not None and users_models.User.objects.filter(
+            pk=attribution_id
+        ).exists() is False:
+            raise ValidationError(
+                "the assigned used does not exists"
             )
-            if not models.Contributor.objects.filter(
+
+        if attribution_id is not None and models.Contributor.objects.filter(
                 project_id=project_id,
-                user=attribution_user,
-            ).exists():
-                raise ValidationError(
-                    "the user you want to assign the issue to is not "
-                    "a contributor of this project"
+                user_id=attribution_id,
+        ).exists() is False:
+            raise ValidationError(
+                "the user you want to assign the issue to is not "
+                "a contributor of this project"
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        reporter = self.context["request"].user
+        project_id = validated_data["project_id"]
+        attribution_id = validated_data.get('attribution_id', None)
+        try:
+            project = models.Project.objects.get(
+                pk=project_id
+            )
+            if attribution_id is not None:
+                assigned_user = users_models.User.objects.get(
+                    pk=attribution_id
                 )
+                validated_data["attribution"] = assigned_user
+        except models.Project.DoesNotExist:
+            raise ValidationError(
+                "the project you give does not exist"
+            )
         except users_models.User.DoesNotExist:
             raise ValidationError(
                 "the user you want to assign the issue to does not exist"
             )
-        validated_data["attribution"] = attribution_user
-        validated_data["reporter"] = reporter_user
+
+        validated_data["reporter"] = reporter
         validated_data["project"] = project
 
         return models.Issue.objects.create(**validated_data)
